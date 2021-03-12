12bb78eac08908355b504f67cd3823345f8a278c
VitaliKlm <kv37813@gmail.com>
2021-03-12 20:43:55 +0300

    feat: implement typical-array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array = []) {
+  return array.reduce(
+    (accumulator, currentValue) => 
+    { return (currentValue < accumulator) ? currentValue : accumulator; }, 
+    0
+  );
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array = []) {
+  return array.reduce(
+    (accumulator, currentValue) => 
+    { return (currentValue > accumulator) ? currentValue : accumulator; }, 
+    0
+  );
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array = []) {
+  if (array.length == 0) return 0; 
+  const sum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 
+    0
+  );
+  return sum/array.length;
+};
